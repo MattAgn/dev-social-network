4d4104d3027df2ea685247c65de9eaad2362799e
mattInnov <mat.alingrin@gmail.com>
2018-07-11 09:20:24 +0200

    Landing done

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white p-4 mt-5 text-center">
+      Copyright &copy; {new Date().getFullYear()}
+    </footer>
+  )
+}
